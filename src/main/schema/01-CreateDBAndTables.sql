7ebd19f1002d2483671890cdacb78c286c501ad9
Ayhan Dardagan <a.a.dardagan@web.de>
2016-10-30 17:49:13 +0200

    Committing running demo project.

---
+++ src/main/schema/01-CreateDBAndTables.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE todo_manager 
+	CHARACTER SET utf8mb4 
+COLLATE utf8mb4_unicode_ci;
+    
+USE todo_manager;
+
+CREATE TABLE users (
+	id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT, 
+	user_name VARCHAR(30) NOT NULL, 
+	email VARCHAR(50) NOT NULL, 
+	pwhash BINARY(60) NOT NULL, 
+	PRIMARY KEY (id),  
+	UNIQUE KEY user_name_uk (user_name),
+    UNIQUE KEY email_uk (email))
+ENGINE = InnoDB;
+
+CREATE TABLE todo_entries (
+	id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT, 
+	user_id INT(11) UNSIGNED NOT NULL,
+    todo_text VARCHAR(100) NOT NULL, 
+	PRIMARY KEY (id),
+	CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE)
+ENGINE = InnoDB;
